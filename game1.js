66742a0003863835b597da69a1ee2337456bde07
khurelbaatar Sumyabazar <Sumyabazar>
2025-10-28 11:14:05 +0800

    game1

--- game1.js
+++ game1.js
@@ -1,21 +1,35 @@
 const readline = require("readline");
+
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-})
-const utga = ["haich", "chuluu", "dawuu"];
-const computer = utga[Math.floor(Math.random() * 3)];
-rl.question("(haich, chuluu, dawuu)", (user) => {
-  console.log("minii songolt: ", user);
-  console.log("computeriin songolt: ", computer);
-if (user === computer) {
-    console.log("tentsew");
-} else if ((user === "chuluu" && computer === "haich") || 
-   (user === "haich" && computer === "dawuu") ||
-   (user === "dawuu" && computer === "chuluu")) {
-   console.log("user ylsan");
-} else {
-    console.log("computer ylsan");
-}
-  rl.close();
+  input: process.stdin,
+  output: process.stdout
 });
+
+function askChoice() {
+  const options = ["haich", "chuluu", "dawuu"];
+  const computer = options[Math.floor(Math.random() * 3)];
+
+  rl.question("ali negiin songono uu haich, chuluu, dawuu: ", (input) => {
+    const userChoice = input.toLowerCase().trim();
+
+    console.log("minii songolt:", userChoice);
+    console.log("computeriin songolt:", computer);
+
+    if (!options.includes(userChoice)) {
+      console.log("zow bichne uu");
+      return askChoice();
+    } else if (userChoice === computer) {
+      console.log("tentsew");
+    } else if (
+      (userChoice === "chuluu" && computer === "haich") ||
+      (userChoice === "haich" && computer === "dawuu") ||
+      (userChoice === "dawuu" && computer === "chuluu")
+    ) {
+      console.log("user ylsan");
+    } else {
+      console.log("computer ylsan");
+    }
+    rl.close();
+  });
+}
+askChoice();
